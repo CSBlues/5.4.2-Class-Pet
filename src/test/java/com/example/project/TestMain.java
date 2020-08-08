e89958ff408e5e176a4bb6e1ebc026a7caea255d
CSBlues <laforests@ashevilleschool.org>
2020-08-08 09:20:05 -0400

    fixed error in testFullConstructor method

--- src/test/java/com/example/project/TestMain.java
+++ src/test/java/com/example/project/TestMain.java
@@ -11,7 +11,7 @@ public class TestMain {
    public void testFullConstructor()
    {
        Pet pet = new Pet("Blue",8,80.0,"dog","Lab");
-       assertEquals( "Blue",pet.getName()),"The full constructor needs to initialize each instance variable with the parameters passed to the constructor method. Make sure you have created the getName() accessor method for the name variable.";
+       assertEquals( "Blue",pet.getName(),"The full constructor needs to initialize each instance variable with the parameters passed to the constructor method. Make sure you have created the getName() accessor method for the name variable.");
        assertEquals( 8,pet.getAge(),"The full constructor needs to initialize each instance variable with the parameters passed to the constructor method. Make sure you have created the getAge() accessor method for the age variable.");
        assertEquals( 80.0, pet.getWeight(), 0.0002, "The full constructor needs to initialize each instance variable with the parameters passed to the constructor method. Make sure you have created the getWeight() accessor method for the weight variable.");
        assertEquals( "dog",pet.getType(), "The full constructor needs to initialize each instance variable with the parameters passed to the constructor method. Make sure you have created the getType() accessor method for the type variable.");
